02a642e4caeaed2b87aca80bbd3d1f1a11666931
pedro henrique silva rodrigues <ph8464803@gmail.com>
2024-01-26 19:37:07 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.print("Alo mundo!");
+
+    }
+
+    public static class Soma{
+        public static void  main(String[] args) {
+            System.out.print(10 + 5);
+        }
+    }
+
+    public static class subtract {
+        public static void main(String[] args) {
+            System.out.print(10 - 5);
+        }
+    }
+}
